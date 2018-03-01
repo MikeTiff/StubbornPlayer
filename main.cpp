c881a977400f0494394b3e042c87b0eee78287f7
MikeTiff <mtiffany@mail.csuchico.edu>
2018-02-28 17:24:49 -0800

    Working, Need to add comments for Project

--- main.cpp
+++ main.cpp
@@ -6,6 +6,23 @@ using namespace std;
 int main()
 {
   SecretDoor game;
+  int round, won=0, count;
+  game.newGame();
+  cout<<"How many times would you like to play?\n";
+  cin>>round;
+  count = round + 1;
+  for(round = 0; round < count ; round++)
+   {
+    game.guessDoorC();
+    game.guessDoorC();
+    if(game.isWinner()==true)
+     {
+      won++; 
+     }
+    game.newGame();
+   
+   }
+  cout<<"You won "<<won<<" times!\n"; 
     
   return 0;
 }
